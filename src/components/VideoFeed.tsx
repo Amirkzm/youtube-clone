4b5404d5492b720a4e27643c07907ca09d4fefc9
Amir Kazemi <kazemi.amir68@gmail.com>
2023-02-10 12:35:25 +0100

    Add VideoFeed section

---
+++ src/components/VideoFeed.tsx
@@ -0,0 +1,13 @@
+import { Stack } from "@mui/material";
+
+interface VideoFeedProps {
+  title: String;
+}
+
+const VideoFeed = () => {
+  return <Stack direction={"row"}></Stack>;
+};
+
+export default VideoFeed;
+
+// AIzaSyBUGsiwwA5aodDhJ78NyCvYKKJJ-tEPZNA
