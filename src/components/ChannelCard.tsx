8ef01129b250851254c7516a67f7fd9585891a5a
Amir Kazemi <kazemi.amir68@gmail.com>
2023-02-12 13:41:09 +0100

    Revert "Add asChannelLogo"
    This reverts commit cae5c5083d165350e7f0f866a21ca9fbcf5d4a95.

--- src/components/ChannelCard.tsx
+++ src/components/ChannelCard.tsx
@@ -9,46 +9,35 @@ import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 
 interface ChannelCardProps {
   channelInfo: any;
-  asChannelLogo?: boolean;
 }
 
-const ChannelCard = ({
-  channelInfo,
-  asChannelLogo = false,
-}: ChannelCardProps) => {
+const ChannelCard = ({ channelInfo }: ChannelCardProps) => {
   return (
-    <Card
-      sx={{
-        width: 340,
-        bgcolor: asChannelLogo ? "common.black" : "Background.paper",
-      }}
-    >
-      <CardMedia
-        component="img"
-        height={asChannelLogo ? 240 : 140}
-        image={channelInfo?.snippet?.thumbnails?.high?.url}
-        alt={channelInfo?.snippet?.title}
-        sx={{
-          borderRadius: asChannelLogo ? "50%" : "0",
-          width: asChannelLogo ? 240 : "100%",
-        }}
-      />
-      <CardContent sx={{ mt: asChannelLogo ? 10 : 0 }}>
-        <Typography gutterBottom variant="h5" component="div">
-          {channelInfo?.snippet?.title}{" "}
-          <CheckCircleIcon
-            sx={{ fontSize: "14px", color: "gray", ml: "5px" }}
-          />
-        </Typography>
-        {channelInfo?.statistics && (
-          <Typography variant="body2" color="text.secondary">
-            {parseInt(channelInfo?.statistics?.subscriberCount).toLocaleString(
-              "en-US"
-            )}{" "}
-            Subscribers
+    <Card sx={{ width: 340 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140"
+          image={channelInfo?.snippet?.thumbnails?.high?.url}
+          alt={channelInfo?.snippet?.title}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {channelInfo?.snippet?.title}{" "}
+            <CheckCircleIcon
+              sx={{ fontSize: "14px", color: "gray", ml: "5px" }}
+            />
           </Typography>
-        )}
-      </CardContent>
+          {channelInfo?.statistic && (
+            <Typography variant="body2" color="text.secondary">
+              {parseInt(
+                channelInfo?.statistics?.subscriberCount
+              ).toLocaleString("en-US")}{" "}
+              Subscribers
+            </Typography>
+          )}
+        </CardContent>
+      </CardActionArea>
     </Card>
   );
 };
