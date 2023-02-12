6c2a9a866c86f08aaec27c0ae3ceb8b423892ff7
Amir Kazemi <kazemi.amir68@gmail.com>
2023-02-12 10:33:41 +0100

    Add routes to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
+import { Box } from "@mui/material";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ChannelDetails from "./components/ChannelDetails";
+import Feed from "./components/Feed";
+import Navbar from "./components/Navbar";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <Box sx={{ backgroundColor: "common.black" }}>
+        <Navbar />
+        <Feed />
+        <Routes>
+          {/* <Route path="/" element={<Feed />} /> */}
+          {/* <Route path="/video/:id" element={<VideoDetail />} /> */}
+          <Route path="/channel/:id" element={<ChannelDetails />} />
+          {/* <Route path="/search/:searchTerm" element={<SearchFeed />} /> */}
+        </Routes>
+      </Box>
+    </BrowserRouter>
+  );
 }
 
 export default App;
