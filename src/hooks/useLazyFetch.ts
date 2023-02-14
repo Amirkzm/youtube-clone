c107da6764dd85f5c72836ac5d7660ba2ab8b382
Amir Kazemi <kazemi.amir68@gmail.com>
2023-02-14 04:03:16 +0100

    update api

--- src/hooks/useLazyFetch.ts
+++ src/hooks/useLazyFetch.ts
@@ -3,7 +3,7 @@ import { useCallback, useState } from "react";
 // const BASE_URL = import.meta.env.VITE_YOUTUBE_API_BASE_URL;
 const BASE_URL = "https://youtube.googleapis.com/youtube/v3/";
 // const key = import.meta.env.VITE_YOUTUBE_API_KEY;
-const key = "AIzaSyDkl3WqZyucx3qv8tCZ7CwDw6nYvySt2xI";
+const key = "AIzaSyDCwM3Ae5wQC231Aai9NyTG4Ds2SF6wphs";
 const credential = `&key=${key}`;
 
 const DEFAULT_OPTIONS = {
