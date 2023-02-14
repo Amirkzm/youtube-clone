72acc24c3ee45877d63854ce88cd4d51cb25c267
Amir Kazemi <kazemi.amir68@gmail.com>
2023-02-14 03:09:03 +0100

    Create component for each comment

---
+++ src/components/videoDetails/CommentItem.tsx
@@ -0,0 +1,24 @@
+import { Avatar, Stack, Typography } from "@mui/material";
+
+interface CommentItemProps {
+  authorAvatar: string;
+  authorName: string;
+  children?: React.ReactNode;
+}
+const CommentItem = ({
+  authorAvatar,
+  authorName,
+  children,
+}: CommentItemProps) => {
+  console.log("commentitemComponent");
+  return (
+    <Stack direction={"row"} justifyContent="flex-start" gap={2} sx={{ mt: 5 }}>
+      <Avatar alt={authorName} src={authorAvatar} />
+      <Typography variant="body1" sx={{ ml: 5 }}>
+        {children}
+      </Typography>
+    </Stack>
+  );
+};
+
+export default CommentItem;
