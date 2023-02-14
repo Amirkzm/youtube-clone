9989f71fc0a37eb66001190537b0137f28f68044
Amir Kazemi <kazemi.amir68@gmail.com>
2023-02-14 04:06:10 +0100

    Add some comment styles

--- src/components/videoDetails/CommentItem.tsx
+++ src/components/videoDetails/CommentItem.tsx
@@ -12,7 +12,12 @@ const CommentItem = ({
 }: CommentItemProps) => {
   console.log("commentitemComponent");
   return (
-    <Stack direction={"row"} justifyContent="flex-start" gap={2} sx={{ mt: 5 }}>
+    <Stack
+      direction={"row"}
+      justifyContent="flex-start"
+      gap={2}
+      sx={{ mt: 5, bgcolor: "background.paper", p: 2, borderRadius: 4 }}
+    >
       <Avatar alt={authorName} src={authorAvatar} />
       <Typography variant="body1" sx={{ ml: 5 }}>
         {children}
