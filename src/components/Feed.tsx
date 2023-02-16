4e91ef22a6bda95bde9fcfc9d9f96311d4069a8d
Amir Kazemi <kazemi.amir68@gmail.com>
2023-02-16 21:36:12 +0100

    Remove Category states and use context instead

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,16 +1,21 @@
-import { Grid, Pagination, Stack } from "@mui/material";
+import { Box, Grid, Pagination, Stack } from "@mui/material";
 import { useEffect, useState } from "react";
+import { useCategoryContext } from "../context/CategoryContext";
 import useLazyFetch from "../hooks/useLazyFetch";
+import PageContainer from "./PageContainer";
 import Sidebar from "./Sidebar";
 import VideoFeed from "./VideoFeed";
 
 const Feed = () => {
-  const [selectedCategory, setSelectedCategory] = useState<string>("New");
+  // const [selectedCategory, setSelectedCategory] = useState<string>("New");
+  const { selectedCategory, setSelectedCategory } = useCategoryContext();
   const [pagitnationCount, setPagitnationCount] = useState<number>(10);
 
-  const changeCategory = (categoryName: string) => {
-    setSelectedCategory(categoryName);
-  };
+  console.log(selectedCategory);
+
+  // const changeCategory = (categoryName: string) => {
+  //   setSelectedCategory(categoryName);
+  // };
 
   const { sendRequest, isLoading, isError, result } =
     useLazyFetch(selectedCategory);
@@ -30,30 +35,34 @@ const Feed = () => {
   }, [result]);
 
   return (
-    <Stack>
-      <Grid container id="gridcontainerfeed" sx={{ width: "100vw" }}>
-        <Grid item xs={2} id="gridSidebar">
-          <Sidebar
-            changeCategory={changeCategory}
-            selectedCategory={selectedCategory}
-          />
+    <PageContainer>
+      <Stack>
+        <Grid container id="gridcontainerfeed" sx={{ width: "100vw" }}>
+          <Box
+            sx={{ display: { xs: "none", lg: "block" } }}
+            component={Grid}
+            item
+            lg={2}
+          >
+            <Sidebar />
+          </Box>
+          <Grid item xs={12} lg={10} sx={{ justifyContent: "center" }}>
+            {!isError && (
+              <VideoFeed
+                category={selectedCategory}
+                result={result}
+                isLoading={isLoading}
+              />
+            )}
+          </Grid>
         </Grid>
-        <Grid item xs={10} sx={{ justifyContent: "center", width: "100%" }}>
-          {!isError && (
-            <VideoFeed
-              category={selectedCategory}
-              result={result}
-              isLoading={isLoading}
-            />
-          )}
-        </Grid>
-      </Grid>
-      <Pagination
-        count={pagitnationCount}
-        color="primary"
-        sx={{ alignSelf: "center", m: "40px" }}
-      />
-    </Stack>
+        <Pagination
+          count={pagitnationCount}
+          color="primary"
+          sx={{ alignSelf: "center", m: "40px" }}
+        />
+      </Stack>
+    </PageContainer>
   );
 };
 
