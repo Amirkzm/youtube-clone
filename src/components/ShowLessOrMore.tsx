957ed1a21d90f36696c70c9dc9a1699249fa9da8
Amir Kazemi <kazemi.amir68@gmail.com>
2023-02-19 12:21:19 +0100

    Complete the Component

--- src/components/ShowLessOrMore.tsx
+++ src/components/ShowLessOrMore.tsx
@@ -1,20 +1,50 @@
-import { Box, Button, Card, Collapse, Stack } from "@mui/material";
-import React, { useEffect, useState } from "react";
-import DOMPurify from "dompurify";
+import { Box, Button, Collapse, Stack } from "@mui/material";
+import { ReactNode, useCallback, useEffect, useRef, useState } from "react";
 
-const ShowLessOrMore = ({ children }: { children?: string }) => {
+interface ShowLessOrMoreProps {
+  children: ReactNode;
+  useCase: "comment" | "description";
+}
+
+const ShowLessOrMore = (props: ShowLessOrMoreProps) => {
+  console.log("inside showlessor more");
+  const { children, useCase } = props;
   const [showMore, setShowMore] = useState<boolean>(false);
-  useEffect(() => {
-    if (children && children.length > 250) {
-      setShowMore(true);
+  const [showCollapse, setShowCollapse] = useState<boolean>(true);
+  const [childrenHeight, setChildrenHeight] = useState<number>(0);
+
+  const measureRef = useCallback((node: HTMLDivElement) => {
+    if (node) {
+      const height = node.getBoundingClientRect().height;
+      setChildrenHeight(height);
     }
-  }, [children]);
+  }, []);
+  //Second-approach
+  // useEffect(() => {
+  //   if (divRef.current) {
+  //     const height = divRef.current.getBoundingClientRect().height;
+  //     if (useCase === "description" && height !== 0 && height < 54) {
+  //       setShowCollapse(false);
+  //     }
+  //     if (useCase === "comment" && height < 80) {
+  //       setShowCollapse(false);
+  //     }
+  //   }
+  // }, [useCase]);
 
-  const summarizedText = showMore ? children : children;
-
-  const sanitizedData = () => ({
-    __html: DOMPurify.sanitize(summarizedText as string),
-  });
+  useEffect(() => {
+    if (
+      useCase === "description" &&
+      childrenHeight !== 0 &&
+      childrenHeight < 45
+    ) {
+      setShowCollapse(false);
+    }
+    if (useCase === "comment" && childrenHeight !== 0 && childrenHeight < 80) {
+      console.log("children height inside useEffect = ", childrenHeight);
+      setShowCollapse(false);
+    }
+  }, [useCase, childrenHeight]);
 
   return (
     <Stack
@@ -22,24 +52,31 @@ const ShowLessOrMore = ({ children }: { children?: string }) => {
         alignItems: "flex-start",
       }}
     >
-      <Collapse
-        in={!showMore}
-        collapsedSize={45}
-        // sx={{
-        //   maxHeight: showMore ? "3em" : "100em",
-        //   overflow: "hidden",
-        //   transition: "all 1s",
-        // }}
-        // dangerouslySetInnerHTML={sanitizedData()}
-      >
-        {children}
-      </Collapse>
-      <Button
-        onClick={() => setShowMore((prev) => !prev)}
-        sx={{ flex: "0 1 auto" }}
-      >
-        {showMore ? "Show more" : "Show less"}
-      </Button>
+      {showCollapse && (
+        <>
+          <Collapse
+            in={showMore}
+            collapsedSize={useCase === "comment" ? 74 : 45}
+          >
+            <div
+              // ref={(node: HTMLDivElement) => {
+              //   return setChildrenHeight(node?.getBoundingClientRect().height);
+              // }}
+              ref={measureRef}
+              // ref={divRef}
+            >
+              {children}
+            </div>
+          </Collapse>
+          <Button
+            onClick={() => setShowMore((prev) => !prev)}
+            sx={{ pl: useCase === "comment" ? 14 : 0 }}
+          >
+            {showMore ? "Show less" : "Show more"}
+          </Button>
+        </>
+      )}
+      {!showCollapse && <Box>{children}</Box>}
     </Stack>
   );
 };
