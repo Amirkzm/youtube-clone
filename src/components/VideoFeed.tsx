236a31e1fcf42f3b7585d82ef485301cf6879102
Amir Kazemi <kazemi.amir68@gmail.com>
2023-02-19 12:19:58 +0100

    Seperate channels and videos

--- src/components/VideoFeed.tsx
+++ src/components/VideoFeed.tsx
@@ -11,54 +11,101 @@ interface VideoFeedProps {
 
 const VideoFeed = (props: VideoFeedProps) => {
   const { category, result, isLoading } = props;
-
   return (
     <Stack sx={{ mt: 10 }}>
-      <Typography variant="h1" sx={{ alignSelf: "start", pl: 5 }}>
-        <Typography
-          component={"span"}
-          variant="h1"
-          sx={{ color: "primary.main" }}
-        >
-          {category}
+      <Stack sx={{ display: isAnyChannel(result) ? "block" : "none" }}>
+        <Typography variant="h1" sx={{ alignSelf: "start", pl: 5 }}>
+          <Typography
+            component={"span"}
+            variant="h1"
+            sx={{ color: "primary.main" }}
+          >
+            {category}
+          </Typography>
+          {" Channels"}
         </Typography>
-        {" Videos"}
-      </Typography>
-      <Stack
-        direction={"row"}
-        flexWrap="wrap"
-        sx={{ mt: 10, gap: 4, justifyContent: "center" }}
-      >
-        {!isLoading &&
-          result?.items.map((item: any, index: number) => {
-            return (
-              <Box key={index}>
-                {item?.id?.channelId && (
-                  <Link to={`/channel/${item?.id?.channelId}`}>
-                    <ChannelCard channelInfo={item} />
-                  </Link>
-                )}
-                {item?.id?.videoId && (
-                  <Link to={`/video/${item?.id?.videoId}`}>
-                    <VideoItem key={item?.id.videoId} videoDetail={item} />
-                  </Link>
-                )}
-              </Box>
-            );
-          })}
-        {isLoading &&
-          new Array(20).fill(1).map((item, index) => {
-            return (
-              <Stack sx={{ width: 340, height: 300, gap: 4 }} key={index}>
-                <Skeleton variant="rectangular" width={340} height={300} />
-                <Skeleton />
-                <Skeleton width="60%" />
-              </Stack>
-            );
-          })}
+
+        <Stack
+          direction={"row"}
+          flexWrap="wrap"
+          sx={{ mt: 10, gap: 4, justifyContent: "center" }}
+        >
+          {!isLoading &&
+            result?.items.map(
+              (item: any, index: number) =>
+                item?.id?.channelId && (
+                  <Box key={index}>
+                    <Link to={`/channel/${item?.id?.channelId}`}>
+                      <ChannelCard channelInfo={item} />
+                    </Link>
+                  </Box>
+                )
+            )}
+          {isLoading &&
+            new Array(20).fill(1).map((item, index) => {
+              return (
+                <Stack sx={{ width: 340, height: 300, gap: 4 }} key={index}>
+                  <Skeleton variant="rectangular" width={340} height={300} />
+                  <Skeleton />
+                  <Skeleton width="60%" />
+                </Stack>
+              );
+            })}
+        </Stack>
+      </Stack>
+      <Stack>
+        <Typography variant="h1" sx={{ alignSelf: "start", pl: 5 }}>
+          <Typography
+            component={"span"}
+            variant="h1"
+            sx={{ color: "primary.main" }}
+          >
+            {category}
+          </Typography>
+          {" Videos"}
+        </Typography>
+
+        <Stack
+          direction={"row"}
+          flexWrap="wrap"
+          sx={{ mt: 10, gap: 4, justifyContent: "center" }}
+        >
+          {!isLoading &&
+            result?.items.map(
+              (item: any, index: number) =>
+                item?.id?.videoId && (
+                  <Box key={index}>
+                    <Link to={`/video/${item?.id?.videoId}`}>
+                      <VideoItem key={item?.id.videoId} videoDetail={item} />
+                    </Link>
+                  </Box>
+                )
+            )}
+          {isLoading &&
+            new Array(20).fill(1).map((item, index) => {
+              return (
+                <Stack sx={{ width: 340, height: 300, gap: 4 }} key={index}>
+                  <Skeleton variant="rectangular" width={340} height={300} />
+                  <Skeleton />
+                  <Skeleton width="60%" />
+                </Stack>
+              );
+            })}
+        </Stack>
       </Stack>
     </Stack>
   );
+};
+
+const isAnyChannel = (result: any) => {
+  let numberOfChannels = 0;
+  const hasAny = result?.items?.some((item: any) => {
+    if (item?.id?.channelId) {
+      return true;
+    }
+  });
+  console.log("isAnychannel=", hasAny);
+  return hasAny;
 };
 
 export default VideoFeed;
