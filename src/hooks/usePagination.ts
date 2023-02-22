a05bafcd8c004d5b919f46eb114a5333fcb1fb48
Amir Kazemi <kazemi.amir68@gmail.com>
2023-02-22 22:05:02 +0100

    Create usePagination hook

---
+++ src/hooks/usePagination.ts
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+
+export interface usePaginationResult {
+  paginationCount: number;
+  currentPage: number;
+  pageToken: string;
+  pageHandler: (event: React.ChangeEvent<unknown>, page: number) => void;
+}
+
+const usePagination = (result: any): usePaginationResult => {
+  const [paginationCount, setPagitnationCount] = useState<number>(10);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [pageToken, setPageToken] = useState<string>("");
+
+  useEffect(() => {
+    if (result) {
+      setPagitnationCount(
+        Math.floor(
+          result?.pageInfo?.totalResults / result?.pageInfo?.resultsPerPage
+        )
+      );
+    }
+  }, [result]);
+
+  const pageHandler = (
+    event: React.ChangeEvent<unknown>,
+    page: number
+  ): void => {
+    if (page > currentPage) {
+      setPageToken(result?.nextPageToken);
+    } else if (page < currentPage) {
+      setPageToken(result?.prevPageToken);
+    }
+    setCurrentPage(page);
+  };
+
+  return { paginationCount, currentPage, pageToken, pageHandler };
+};
+
+export default usePagination;
