8a27665e623f5693981b11cea09307ff7b1e8bf0
Amir Kazemi <kazemi.amir68@gmail.com>
2023-02-22 21:25:41 +0100

    Move to pages folder

---
+++ src/pages/Feed.tsx
@@ -0,0 +1,81 @@
+import { Box, Grid, Pagination, Stack } from "@mui/material";
+import { useEffect, useState } from "react";
+import { useCategoryContext } from "../context/CategoryContext";
+import useLazyFetch from "../hooks/useLazyFetch";
+import PageContainer from "../components/PageContainer";
+import Sidebar from "../components/layouts/Sidebar";
+import VideoFeed from "../components/videos/VideoFeed";
+
+const Feed = () => {
+  const { selectedCategory, setSelectedCategory } = useCategoryContext();
+  const [pagitnationCount, setPagitnationCount] = useState<number>(10);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [pageToken, setPageToken] = useState<string>("");
+
+  const { sendRequest, isLoading, isError, result } =
+    useLazyFetch(selectedCategory);
+
+  useEffect(() => {
+    if (currentPage === 1) {
+      sendRequest(`search?part=snippet&maxResults=20&q=${selectedCategory}`);
+    } else {
+      sendRequest(
+        `search?part=snippet&maxResults=20&q=${selectedCategory}&pageToken=${result?.nextPageToken}`
+      );
+    }
+  }, [pageToken, currentPage, selectedCategory]);
+
+  useEffect(() => {
+    if (result) {
+      setPagitnationCount(
+        Math.floor(
+          result?.pageInfo?.totalResults / result?.pageInfo?.resultsPerPage
+        )
+      );
+    }
+  }, [result]);
+
+  const PageHandler = (event: React.ChangeEvent<unknown>, page: number) => {
+    if (page > currentPage) {
+      setPageToken(result?.nextPageToken);
+    } else if (page < currentPage) {
+      setPageToken(result?.prevPageToken);
+    }
+    setCurrentPage(page);
+  };
+
+  return (
+    <PageContainer>
+      <Stack>
+        <Grid container id="gridcontainerfeed" sx={{ width: "100vw" }}>
+          <Box
+            sx={{ display: { xs: "none", lg: "block" } }}
+            component={Grid}
+            item
+            lg={2}
+          >
+            <Sidebar />
+          </Box>
+          <Grid item xs={12} lg={10} sx={{ justifyContent: "center" }}>
+            {!isError && (
+              <VideoFeed
+                selectedCategory={selectedCategory}
+                result={result}
+                isLoading={isLoading}
+              />
+            )}
+          </Grid>
+        </Grid>
+        <Pagination
+          count={pagitnationCount}
+          color="primary"
+          sx={{ alignSelf: "center", m: "40px" }}
+          page={currentPage}
+          onChange={PageHandler}
+        />
+      </Stack>
+    </PageContainer>
+  );
+};
+
+export default Feed;
