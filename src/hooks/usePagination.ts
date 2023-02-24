af137399a0a84ebb8891c364c998a7fb63c72a26
Amir Kazemi <kazemi.amir68@gmail.com>
2023-02-24 12:49:54 +0100

    Remove paginationCount

--- src/hooks/usePagination.ts
+++ src/hooks/usePagination.ts
@@ -1,26 +1,14 @@
 import { useEffect, useState } from "react";
 
 export interface usePaginationResult {
-  paginationCount: number;
   currentPage: number;
   pageToken: string;
   pageHandler: (page: number) => void;
 }
 
 const usePagination = (result: any): usePaginationResult => {
-  const [paginationCount, setPagitnationCount] = useState<number>(10);
   const [currentPage, setCurrentPage] = useState<number>(1);
   const [pageToken, setPageToken] = useState<string>("");
-
-  useEffect(() => {
-    if (result) {
-      setPagitnationCount(
-        Math.floor(
-          result?.pageInfo?.totalResults / result?.pageInfo?.resultsPerPage
-        )
-      );
-    }
-  }, [result]);
 
   const pageHandler = (page: number): void => {
     if (page > currentPage) {
@@ -31,7 +19,7 @@ const usePagination = (result: any): usePaginationResult => {
     setCurrentPage(page);
   };
 
-  return { paginationCount, currentPage, pageToken, pageHandler };
+  return { currentPage, pageToken, pageHandler };
 };
 
 export default usePagination;
