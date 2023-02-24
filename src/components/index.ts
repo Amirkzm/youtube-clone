19276bc2a45ddaeae4374b091ae5bae356d6dc2b
Amir Kazemi <kazemi.amir68@gmail.com>
2023-02-24 12:42:29 +0100

    Add pagination

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,8 @@
 export { default as PageContainer } from "./PageContainer";
 export { default as Search } from "./Search";
 export { default as ShowLessOrMore } from "./ShowLessOrMore";
+export { default as Pagination } from "./Pagination";
+
 export * from "./layouts";
 export * from "./channels";
 export * from "./comments";
