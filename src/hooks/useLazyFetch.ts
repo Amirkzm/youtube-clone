0f0222da367a89cbb141b495c5d27e23218e89fe
Amir Kazemi <kazemi.amir68@gmail.com>
2023-02-24 22:21:44 +0100

    Use env variables

--- src/hooks/useLazyFetch.ts
+++ src/hooks/useLazyFetch.ts
@@ -1,9 +1,9 @@
 import { useCallback, useState } from "react";
 
-// const BASE_URL = import.meta.env.VITE_YOUTUBE_API_BASE_URL;
-const BASE_URL = "https://youtube.googleapis.com/youtube/v3/";
-// const key = import.meta.env.VITE_YOUTUBE_API_KEY;
-const key = "AIzaSyDCwM3Ae5wQC231Aai9NyTG4Ds2SF6wphs";
+const BASE_URL = import.meta.env.VITE_YOUTUBE_API_BASE_URL;
+// const BASE_URL = "https://youtube.googleapis.com/youtube/v3/";
+const key = import.meta.env.VITE_YOUTUBE_API_KEY;
+// const key = "AIzaSyDCwM3Ae5wQC231Aai9NyTG4Ds2SF6wphs";
 const credential = `&key=${key}`;
 
 const DEFAULT_OPTIONS = {
